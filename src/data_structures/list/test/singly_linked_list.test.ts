d5ffc09ec83b4c987730b82ccd062e6c6d866073
Rojan Rana Magar <rozenmagar058@gmail.com>
2025-12-22 11:07:11 +0545

    initial project

---
+++ src/data_structures/list/test/singly_linked_list.test.ts
@@ -0,0 +1,5 @@
+import { describe } from "vitest";
+import { SinglyLinkedList } from "../singly_linked_list";
+import { testLinkedList } from "./linked_list";
+
+describe("Singly linked list", () => testLinkedList(SinglyLinkedList));
